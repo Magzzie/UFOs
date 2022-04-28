292bd466811a62fe627cb60aaa65a4f5099586f0
Magzzie <99442686+Magzzie@users.noreply.github.com>
2022-04-27 23:38:05 -0400

    Built table and date filter on click.

--- static/js/app.js
+++ static/js/app.js
@@ -12,17 +12,51 @@ function buildTable(data) {
     // Next, loop through each object in the data
     // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
-        // Append a row to the table body
+        // Append a row to the table body ('table row')
        let row = tbody.append("tr");
 
        // Loop through each field in the dataRow and add
-       // each value as a table cell (td)
+       // each value as a table cell (td) ('tabe data')
        Object.values(dataRow).forEach((val) => {
            let cell = row.append("td");
            cell.text(val);
-       }) ;
+       });
     });
-}
+};
+
+// Create a function to filter the table on date after user's click.
+function handleClick() {
+    // Select the datetime id and grab that info and hold it in a variable
+    // (Grab the datetime value from the filter)
+    let date = d3.select("#datetime").property("value");
+    
+    // Set a default filter and save it to a new variable.
+    // default filter is the original table data (raw data) because we want  
+    // users to refine search on their own terms. 
+    // If no date has been entered by user as a filter, then all of the data will be returned instead.
+    let filteredData = tableData;
+
+    // Chack to see if a date was entered and filter the 
+    // data using that date.
+    if (date) {
+        // Apply `filter` to the table data to only keep the 
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data
+    // @NOTE: if no date was entered, then filteredData will
+    // just be the original tableDate. 
+    buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button.
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Call the original table to build the table when the page loads.
+buildTable(tableData);
+
+
 
 
 
